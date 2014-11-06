e91eb785ccdf7019d3019bd4cc579011ae27e185
Matheus Inácio <matheus.bs@ic.ufal.br>
2014-11-06 16:09:32 -0200

    MainGui + Aulas

--- src/gui/mainGui.java
+++ src/gui/mainGui.java
@@ -5,23 +5,23 @@
  */
 package gui;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
+import apresentacao.FormCadastroMateria;
 import java.beans.PropertyVetoException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JDesktopPane;
+import apresentacao.FormCadastroProfessor;
+import apresentacao.FormSelectHorario;
 
 /**
  *
  * @author matheus
  */
-public class guiMain extends javax.swing.JFrame {
+public class mainGui extends javax.swing.JFrame {
 
     /**
-     * Creates new form guiMain
+     * Creates new form mainGui
      */
-    public guiMain() {
+    public mainGui() {
         initComponents();
         this.setExtendedState(MAXIMIZED_BOTH);
     }
@@ -35,42 +35,22 @@ public class guiMain extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jMenu3 = new javax.swing.JMenu();
-        jMenu4 = new javax.swing.JMenu();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTree1 = new javax.swing.JTree();
         jButtonNovoProfessor = new javax.swing.JButton();
-        jButtonListProfessor = new javax.swing.JButton();
         jButtonHorario = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jButtonUltimosHorarios = new javax.swing.JButton();
         jButtonNovaMateria = new javax.swing.JButton();
-        jButtonListMateria = new javax.swing.JButton();
         jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         JDesktopMDI = new javax.swing.JDesktopPane();
-
-        jMenu3.setText("jMenu3");
-
-        jMenu4.setText("jMenu4");
-
-        jScrollPane1.setViewportView(jTree1);
+        jButtonSelectHoras = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        setPreferredSize(new java.awt.Dimension(0, 0));
 
-        jButtonNovoProfessor.setText("Novo Professor");
+        jButtonNovoProfessor.setText("Professores");
         jButtonNovoProfessor.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButtonNovoProfessorActionPerformed(evt);
-            }
-        });
-
-        jButtonListProfessor.setText("Lista de Professores");
-        jButtonListProfessor.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonListProfessorActionPerformed(evt);
             }
         });
 
@@ -90,17 +70,10 @@ public class guiMain extends javax.swing.JFrame {
             }
         });
 
-        jButtonNovaMateria.setText("Nova Materia");
+        jButtonNovaMateria.setText("Materias");
         jButtonNovaMateria.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButtonNovaMateriaActionPerformed(evt);
-            }
-        });
-
-        jButtonListMateria.setText("Lista de Materias");
-        jButtonListMateria.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonListMateriaActionPerformed(evt);
             }
         });
 
@@ -113,7 +86,7 @@ public class guiMain extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(223, 223, 223)
                 .addComponent(jLabel2)
-                .addContainerGap(229, Short.MAX_VALUE))
+                .addContainerGap(201, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -122,6 +95,13 @@ public class guiMain extends javax.swing.JFrame {
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(19, Short.MAX_VALUE))
         );
+
+        jButtonSelectHoras.setText("Selecionar Horas");
+        jButtonSelectHoras.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonSelectHorasActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -134,12 +114,11 @@ public class guiMain extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jLabel1)
-                            .addComponent(jButtonListProfessor, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jButtonNovoProfessor, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jButtonNovaMateria, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButtonListMateria, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jButtonHorario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButtonUltimosHorarios, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(jButtonUltimosHorarios, javax.swing.GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
+                            .addComponent(jButtonSelectHoras, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(32, 32, 32)
                         .addComponent(JDesktopMDI)))
                 .addContainerGap())
@@ -154,16 +133,14 @@ public class guiMain extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jButtonNovoProfessor)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jButtonListProfessor)
+                        .addComponent(jButtonSelectHoras)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jButtonNovaMateria)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jButtonListMateria)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jButtonHorario)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jButtonUltimosHorarios)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 153, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 169, Short.MAX_VALUE)
                         .addComponent(jLabel1))
                     .addComponent(JDesktopMDI))
                 .addContainerGap())
@@ -171,42 +148,36 @@ public class guiMain extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+    private void theButton() {
+        JDesktopMDI.removeAll();
+        JDesktopMDI.repaint();
+        jButtonNovoProfessor.setEnabled(true);
+        jButtonHorario.setEnabled(true);
+        jButtonUltimosHorarios.setEnabled(true);
+        jButtonNovaMateria.setEnabled(true);
+        jButtonSelectHoras.setEnabled(true);
 
+    }
     private void jButtonNovoProfessorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonNovoProfessorActionPerformed
         // TODO add your handling code here:
         theButton();
-        jButtonNovoProfessor.setEnabled(false);
-        JFrameTeste frame = null;
-        try {
-            frame = new JFrameTeste();
+        FormCadastroProfessor newProf = null;
+        theButton();
+        try{
+            newProf = new FormCadastroProfessor();
         } catch (PropertyVetoException ex) {
-            Logger.getLogger(guiMain.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(mainGui.class.getName()).log(Level.SEVERE, null, ex);
         }
-        frame.setVisible(true);
-        JDesktopMDI.add(frame);
-        
+        newProf.setVisible(true);
+        JDesktopMDI.add(newProf);
+        try{
+            newProf.setMaximum(true);
+        } catch (PropertyVetoException ex) {
+            Logger.getLogger(mainGui.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        jButtonNovoProfessor.setEnabled(false);
 
     }//GEN-LAST:event_jButtonNovoProfessorActionPerformed
-
-    private void jButtonListProfessorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonListProfessorActionPerformed
-        // TODO add your handling code here:
-        
-        theButton();
-        jButtonListProfessor.setEnabled(false);
-    }//GEN-LAST:event_jButtonListProfessorActionPerformed
-
-    private void jButtonNovaMateriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonNovaMateriaActionPerformed
-        // TODO add your handling code here:
-        
-        theButton();
-        jButtonNovaMateria.setEnabled(false);
-    }//GEN-LAST:event_jButtonNovaMateriaActionPerformed
-
-    private void jButtonListMateriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonListMateriaActionPerformed
-        // TODO add your handling code here:
-        theButton();
-        jButtonListMateria.setEnabled(false);
-    }//GEN-LAST:event_jButtonListMateriaActionPerformed
 
     private void jButtonHorarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonHorarioActionPerformed
         // TODO add your handling code here:
@@ -219,17 +190,40 @@ public class guiMain extends javax.swing.JFrame {
         theButton();
         jButtonUltimosHorarios.setEnabled(false);
     }//GEN-LAST:event_jButtonUltimosHorariosActionPerformed
-    private void theButton() {
-        JDesktopMDI.removeAll();        
-        JDesktopMDI.repaint();
-        jButtonNovoProfessor.setEnabled(true);
-        jButtonListProfessor.setEnabled(true);
-        jButtonHorario.setEnabled(true);
-        jButtonUltimosHorarios.setEnabled(true);
-        jButtonNovaMateria.setEnabled(true);
-        jButtonListMateria.setEnabled(true);
 
-    }
+    private void jButtonNovaMateriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonNovaMateriaActionPerformed
+        // TODO add your handling code here:
+        FormCadastroMateria newMateria = null ;
+        theButton();
+        try{
+            newMateria = new FormCadastroMateria();
+        } catch (PropertyVetoException ex) {
+            Logger.getLogger(mainGui.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        newMateria.setVisible(true);
+        jButtonNovaMateria.setEnabled(false);
+        JDesktopMDI.add(newMateria);
+        try{
+            newMateria.setMaximum(true);
+        } catch (PropertyVetoException ex) {
+            Logger.getLogger(mainGui.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButtonNovaMateriaActionPerformed
+
+    private void jButtonSelectHorasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonSelectHorasActionPerformed
+        // TODO add your handling code here:
+        FormSelectHorario horas = null;
+        theButton();
+        horas = new FormSelectHorario();
+        horas.setVisible(true);
+        jButtonSelectHoras.setEnabled(false);
+        JDesktopMDI.add(horas);
+        try{
+            horas.setMaximum(true);
+        } catch (PropertyVetoException ex) {
+            Logger.getLogger(mainGui.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButtonSelectHorasActionPerformed
 
     /**
      * @param args the command line arguments
@@ -248,20 +242,20 @@ public class guiMain extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(guiMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(mainGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(guiMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(mainGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(guiMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(mainGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(guiMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(mainGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new guiMain().setVisible(true);
+                new mainGui().setVisible(true);
             }
         });
     }
@@ -269,17 +263,12 @@ public class guiMain extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JDesktopPane JDesktopMDI;
     private javax.swing.JButton jButtonHorario;
-    private javax.swing.JButton jButtonListMateria;
-    private javax.swing.JButton jButtonListProfessor;
     private javax.swing.JButton jButtonNovaMateria;
     private javax.swing.JButton jButtonNovoProfessor;
+    private javax.swing.JButton jButtonSelectHoras;
     private javax.swing.JButton jButtonUltimosHorarios;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JMenu jMenu3;
-    private javax.swing.JMenu jMenu4;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTree jTree1;
     // End of variables declaration//GEN-END:variables
 }
